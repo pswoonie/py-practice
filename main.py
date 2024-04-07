d55883585c5bc952adc9b2cb6a6a86ff806c12b6
pswoonie <swp8111@gmail.com>
2024-04-07 21:02:55 +0900

    [UPDATE] __init__.py practice

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import modules.my_module as mymo
 # from modules.my_module import *
 # from modules.my_module import my_func, PI
+from packages.image.io_file import imgread as img
 
 # print(mymo.PI)
 mymo.my_func()
 # print(PI)
 # my_func()
+img.pngread()
+img.jpgread()
